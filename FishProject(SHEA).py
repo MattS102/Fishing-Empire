08031f38a802adb1e107717234dd707cd06b206c
owenchen0408 <owenchen0408@gmail.com>
2023-07-17 15:08:32 -0400

    add show logo and basic pygame start

--- FishProject(SHEA).py
+++ FishProject(SHEA).py
@@ -4,8 +4,28 @@ import pygame
 	
 # Define Main function
 def main():
+	# initialize the pygame module
+	pygame.init()
+	# load and set the logo
+	logo = pygame.image.load("logo32x32.png")
+	pygame.display.set_icon(logo)
+	pygame.display.set_caption("minimal program")
 
-	# Set up starting variables
+	# create a surface on screen that has the size of 240 x 180
+	screen = pygame.display.set_mode((240,180))
+
+	# define a variable to control the main loop
+	running = True
+
+	# main loop
+	while running:
+		# event handling, gets all event from the event queue
+		for event in pygame.event.get():
+		# only do something if the event is of type QUIT
+			if event.type == pygame.QUIT:
+				# change the value to False, to exit the main loop
+				running = False
+# Set up starting variables
 	mainRod = "Fishing Rod (Level 1)"
 	mainPowerUp = ""
 	totalPoints = 0
