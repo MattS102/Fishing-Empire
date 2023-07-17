9d4d5d1985c0185b7d6e1a7522da6ffca5e84de6
timoslater <tsla0307@gmail.com>
2023-07-17 09:48:58 -0400

    Inherit pygame.sprite.Sprite for each class, Add Fish Rarity with weighted selection

--- classes.py
+++ classes.py
@@ -1,3 +1,28 @@
-class Fish:
+import pygame
+from numpy.random import choice as weighted_choice
+
+
+class Fish(pygame.sprite.Sprite):
+    RARITY_DICT = {
+        "Common": 5,
+        "Uncommon": 10,
+        "Rare": 30,
+        "Very Rare": 50,
+        "Exotic": 100,
+        "Black Market": 200,
+    }
+
+    # Rocket Leauge-type rarity tiers as keys and prices as values
+
     def __init__(self) -> None:
-        pass
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((50, 40))
+        self.rarity = weighted_choice(
+            Fish.RARITY_DICT.keys(), p=[0.35, 0.25, 0.20, 0.15, 0.05]
+        )
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self) -> None:
+        pygame.sprite.Sprite.__init__(self)
+        self.inventory = []
